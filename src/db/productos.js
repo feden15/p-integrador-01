efb207b3f37384d7858d21b4a4d4904b0fb587b5
Federico Nova <fedenova8@gmail.com>
2025-02-13 16:49:21 -0300

    Agregamos más productos para que quede más vistozo

--- src/db/productos.js
+++ src/db/productos.js
@@ -46,6 +46,18 @@ const productos = [
         nombre: "Porta vasos",
         foto: "imgs/porta-vasos.webp",
         descripcion: "Decorativo y funcional, diseñado para proteger superficies, elaborado artesanalmente con cerámica y yeso, aportando estilo y originalidad a cualquier espacio."
+    },
+    {
+        id: 9,
+        nombre: "Huevera",
+        foto: "imgs/huevera.webp",
+        descripcion: "La clásica huevera, un utensilio artesanal diseñado para sostener y organizar huevos, elaborado con cerámica y yeso, combinando funcionalidad y estética en la cocina o decoración."
+    },
+    {
+        id: 10,
+        nombre: "Porta vasos",
+        foto: "imgs/porta-vasos.webp",
+        descripcion: "Decorativo y funcional, diseñado para proteger superficies, elaborado artesanalmente con cerámica y yeso, aportando estilo y originalidad a cualquier espacio."
     }
 ]
 
